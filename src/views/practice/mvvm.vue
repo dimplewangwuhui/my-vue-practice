5a4d439bc78d032758f340cd2f32bcfd9e867e5d
wuhui.wang <wuhui.wang@capitalonline.net>
2019-11-25 18:17:25 +0800

    my-vue-practice

--- src/views/practice/mvvm.vue
+++ src/views/practice/mvvm.vue
@@ -102,7 +102,7 @@
                 tableData: [
                     {key:'.lazy', value:'在默认情况下，v-model 在每次 input 事件触发后将输入框的值与数据进行同步，你可以添加 lazy 修饰符，从而转变为使用 change 事件进行同步。', message:''},
                     {key:'.number', value:'如果想自动将用户的输入值转为数值类型，可以给 v-model 添加 number 修饰符。这通常很有用，因为即使在 type="number" 时，HTML 输入元素的值也总会返回字符串。如果这个值无法被 parseFloat() 解析，则会返回原始的值。', message:''},
-                    {key:'.trim', value:'如果要自动过滤用户输入的首尾空白字符，可以给 v-model 添加 trim 修饰符。', message:''},
+                    {key:'.trim', value:'如果要自动过滤用户输入的首尾空白字符，可以给 v-model 添加 trim 修饰符。.', message:''},
                 ]
             }
         },
